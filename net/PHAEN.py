4568151fe8a46ab8415857413480783672edf070
CrazyAn-JL <jianlongan@163.com>
2025-12-11 14:52:47 +0800

    Update PHAEN.py

--- net/PHAEN.py
+++ net/PHAEN.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 
-
 class PhaseRefine(nn.Module):
     def __init__(self, nc=3):
         super().__init__()
@@ -9,9 +8,7 @@ class PhaseRefine(nn.Module):
             nn.Conv2d(nc, nc, 1, 1, 0),
             nn.LeakyReLU(0.1, inplace=True),
             nn.Conv2d(nc, nc, 1, 1, 0),
-            nn.Tanh()  # 相位微调 [-π, π] 范围
+            nn.Tanh()
         )
-
     def forward(self, pha):
         return pha + 0.1 * self.refine(pha)
-
